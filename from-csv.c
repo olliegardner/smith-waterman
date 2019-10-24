bb6f435e7f525b1e9dbd7e6ca87848a842b54f6e
Michel Steuwer <michel.steuwer@glasgow.ac.uk>
2019-10-24 13:35:58 +0100

    Update from-csv.c

--- from-csv.c
+++ from-csv.c
@@ -8,7 +8,9 @@ int main(int argc, char **argv)
 {
     // first command line argument (stored in argv[1]) is the csv filename
     // open the csv file
-    // load in csv line-by-line (ignoring lines starting by #)
+    // load in csv line-by-line (ignoring lines starting by #;
+    //                           you can assume that a line will be never
+    //                           longer than 256 characters)
     // for each line:
     //   read string up to `,' as the first string
     //   read string after `,' up to the newline character as the second string
